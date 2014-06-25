43edb8ee6d7e788017cde2ba62736cfadf1b3251
John Dapzar <johndapzar@yahoo.com>
2014-06-25 14:22:26 +0530

    Config

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'zote_db',
 			'username'  => 'root',
-			'password'  => 'zakth3ild0',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
